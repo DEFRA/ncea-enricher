8fbe815cfe39ace401831f93bd675c1be16025de
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-04-11 23:36:40 +0200

    refactor serach service

--- src/ncea-enricher/Services/Contracts/IXmlSearchService.cs
+++ src/ncea-enricher/Services/Contracts/IXmlSearchService.cs
@@ -1,9 +1,6 @@
-﻿using System.Xml.Linq;
-
-namespace Ncea.Enricher.Services.Contracts;
+﻿namespace Ncea.Enricher.Services.Contracts;
 
 public interface IXmlSearchService
 {
     bool IsMatchFound(string value, List<string> synonyms);
-    bool IsMatchFound(XElement root, string elementPath, List<string> synonyms);
 }
--- src/ncea-enricher/Services/XmlSearchService.cs
+++ src/ncea-enricher/Services/XmlSearchService.cs
@@ -1,40 +1,15 @@
 ﻿using Ncea.Enricher.Services.Contracts;
 using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using System.Xml.XPath;
 
 namespace Ncea.Enricher.Services
 {
     public class XmlSearchService : IXmlSearchService
     {
-        public bool IsMatchFound(XElement root, string elementPath, List<string> synonyms)
-        {
-            var rgx = new Regex(@"\b(" + string.Join("|", synonyms.Select(Regex.Escape).ToArray()) + @"\b)");
-            var element = ElementAtPath(root, elementPath);
-            var matchCollection = rgx.Matches(element.Value);
-            return matchCollection.Count > 0;
-        }
-
         public bool IsMatchFound(string value, List<string> synonyms)
         {
             var rgx = new Regex(@"\b" + string.Join("|", synonyms.Select(Regex.Escape).ToArray()) + @"\b");
             var matchCollection = rgx.Matches(value);
             return matchCollection.Count > 0;
-        }
-
-        private static XElement ElementAtPath(XElement root, string path)
-        {
-            if (root == null)
-            {
-                throw new ArgumentNullException(nameof(root));
-            }
-
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                throw new ArgumentException("Invalid path.");
-            }
-
-            return root.XPathSelectElement(path)!;
         }
     }
 }
