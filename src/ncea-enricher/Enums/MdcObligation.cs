6d3f2e116bdbeda3f91b138eea04e4e638f845ea
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-05-15 18:08:55 +0200

    Added new resource type

--- src/ncea-enricher/Enums/MdcObligation.cs
+++ src/ncea-enricher/Enums/MdcObligation.cs
@@ -2,8 +2,8 @@
 
 public enum MdcObligation
 {
-    Mandatory = 1,
-    Conditional = 2,
-    Optional = 3,
-    MandatoryAndConditional = 4,
+    Mandatory,
+    Conditional,
+    Optional,
+    MandatoryAndConditional,
 }
