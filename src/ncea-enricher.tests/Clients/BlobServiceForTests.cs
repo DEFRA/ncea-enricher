5abfe46e4a7e19e262bef6b21dc22c1f3a7e3ba0
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-05-24 00:29:32 +0200

    utc fies

--- src/ncea-enricher.tests/Clients/BlobServiceForTests.cs
+++ src/ncea-enricher.tests/Clients/BlobServiceForTests.cs
@@ -28,6 +28,55 @@ public static class BlobServiceForTests
         var service = new BlobService(mockBlobServiceClient.Object);
         return service;
     }
+    public static BlobService Get(out Mock<BlobServiceClient> mockBlobServiceClient,
+                                  out Mock<BlobContainerClient> mockBlobContainerClient,
+                                  out Mock<BlobClient> mockBlobClient)
+    {
+        mockBlobClient = new Mock<BlobClient>();
+        mockBlobClient.Setup(x => x.Uri).Returns(new Uri(new Uri("https://base-uri-blob-storage"), "relative-uri-blob-storage"));
+
+        var blobContent = new BinaryData("this is test data");
+        var downloadResult = BlobsModelFactory.BlobDownloadResult(content: blobContent);
+
+        // Empty Response mock to merge with since I only care about the downloadResult. Modify at will.
+        var response = Response.FromValue(downloadResult, new Mock<Response>().Object);
+
+        mockBlobClient.Setup(x =>
+            x.DownloadContentAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(response));
+
+        mockBlobServiceClient = new Mock<BlobServiceClient>();
+        mockBlobContainerClient = new Mock<BlobContainerClient>();
+
+        var blobContainerInfo = BlobsModelFactory.BlobContainerInfo(It.IsAny<ETag>(), It.IsAny<DateTimeOffset>());
+        var mockContainerResponse = Response.FromValue(blobContainerInfo, new Mock<Response>().Object);
+
+        mockBlobServiceClient.Setup(x => x.GetBlobContainerClient(It.IsAny<string>())).Returns(mockBlobContainerClient.Object);
+        mockBlobContainerClient.Setup(x => x.GetBlobClient(It.IsAny<string>())).Returns(mockBlobClient.Object);
+        mockBlobContainerClient.Setup(x => x.ExistsAsync(It.IsAny<CancellationToken>())).ReturnsAsync(Response.FromValue<bool>(true, Mock.Of<Response>()));
+        mockBlobContainerClient.Setup<Task<Response<BlobContainerInfo>>>(x =>
+            x.CreateIfNotExistsAsync(It.IsAny<PublicAccessType>(), It.IsAny<IDictionary<string, string>>(),
+            It.IsAny<BlobContainerEncryptionScopeOptions>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockContainerResponse);
+        mockBlobContainerClient.Setup(x =>
+            x.DeleteBlobIfExistsAsync(It.IsAny<string>(), It.IsAny<DeleteSnapshotsOption>(),
+            It.IsAny<BlobRequestConditions>(), It.IsAny<CancellationToken>())).ReturnsAsync(Response.FromValue<bool>(true, new Mock<Response>().Object));
+
+        var blobList = new BlobItem[]
+        {
+            BlobsModelFactory.BlobItem("Blob1"),
+            BlobsModelFactory.BlobItem("Blob2"),
+            BlobsModelFactory.BlobItem("Blob3")
+        };
+        Page<BlobItem> page = Page<BlobItem>.FromValues(blobList, null, Mock.Of<Response>());
+        AsyncPageable<BlobItem> pageableBlobList = AsyncPageable<BlobItem>.FromPages(new[] { page });
+        mockBlobContainerClient
+            .Setup(m => m.GetBlobsAsync(It.IsAny<BlobTraits>(), It.IsAny<BlobStates>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .Returns(pageableBlobList);
+
+        var service = new BlobService(mockBlobServiceClient.Object);
+
+        return service;
+    }
 
     public static BlobService GetMdcXml()
     {
--- src/ncea-enricher.tests/Infrastructure/BlobServiceTests.cs
+++ src/ncea-enricher.tests/Infrastructure/BlobServiceTests.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using Azure.Storage.Blobs;
+using FluentAssertions;
 using Moq;
+using Ncea.Enricher.Infrastructure.Models.Requests;
 using Ncea.Enricher.Tests.Clients;
 using System.Data;
 
@@ -20,5 +22,22 @@ public class BlobServiceTests
         result.Should().NotBeNull();
         result.Should().BeOfType<DataTable>();
         result.Rows.Should().HaveCount(117);
+    }
+
+    [Fact]
+    public async Task GetContentAsync_WhenBlobsFromHarvesterExists_ReadTheContnetFromBlob()
+    {
+        // Arrange
+        var service = BlobServiceForTests.Get(out Mock<BlobServiceClient> mockBlobServiceClient,
+                                              out Mock<BlobContainerClient> mockBlobContainerClient,
+                                              out Mock<BlobClient> mockBlobClient);
+
+        // Act
+        await service.GetContentAsync(new GetBlobContentRequest(It.IsAny<string>(), It.IsAny<string>()), It.IsAny<CancellationToken>());
+
+        // Assert
+        mockBlobServiceClient.Verify(x => x.GetBlobContainerClient(It.IsAny<string>()), Times.Exactly(1));
+        mockBlobContainerClient.Verify(x => x.GetBlobClient(It.IsAny<string>()), Times.Exactly(1));
+        mockBlobClient.Verify(x => x.DownloadContentAsync(It.IsAny<CancellationToken>()), Times.Once);
     }
 }
