54cd0ae65928abf74bdd335e2ac2d8be7763144f
Ganesh <GRajamahant@anglianwater.co.uk>
2024-09-04 14:09:26 +0100

    Fixed Sonar issues

--- src/ncea-enricher/Constants/TrainedModels.cs
+++ src/ncea-enricher/Constants/TrainedModels.cs
@@ -4,30 +4,30 @@ namespace Ncea.Enricher.Constants;
 
 public static class TrainedModels
 {
-    public static string AssetTrainedModel = "AssetTrainedModel";
-    public static string BenefitTrainedModel = "BenefitTrainedModel";
-    public static string PreassureTrainedModel = "PreassureTrainedModel";
-    public static string ValuationTrainedModel = "ValuationTrainedModel";
+    public const string AssetTrainedModel = "AssetTrainedModel";
+    public const string BenefitTrainedModel = "BenefitTrainedModel";
+    public const string PreassureTrainedModel = "PreassureTrainedModel";
+    public const string ValuationTrainedModel = "ValuationTrainedModel";
 
-    public static string lvl1_001 = "lvl1-001";
-    public static string lvl1_002 = "lvl1-002";
-    public static string lvl1_003 = "lvl1-003";
-    public static string lvl1_004 = "lvl1-004";
+    public const string lvl1_001 = "lvl1-001";
+    public const string lvl1_002 = "lvl1-002";
+    public const string lvl1_003 = "lvl1-003";
+    public const string lvl1_004 = "lvl1-004";
 
-    public static string lv2_001 = "lv2-001";
-    public static string lv2_002 = "lv2-002";
-    public static string lv2_003 = "lv2-003";
-    public static string lv2_004 = "lv2-004";
-    public static string lv2_005 = "lv2-005";
-    public static string lv2_006 = "lv2-006";
-    public static string lv2_007 = "lv2-007";
-    public static string lv2_008 = "lv2-008";
-    public static string lv2_009 = "lv2-009";
-    public static string lv2_010 = "lv2-010";
-    public static string lv2_011 = "lv2-011";
-    public static string lv2_012 = "lv2-012";
-    public static string lv2_013 = "lv2-013";
-    public static string lv2_014 = "lv2-014";
-    public static string lv2_015 = "lv2-015";
-    public static string lv2_016 = "lv2-016";
+    public const string lv2_001 = "lv2-001";
+    public const string lv2_002 = "lv2-002";
+    public const string lv2_003 = "lv2-003";
+    public const string lv2_004 = "lv2-004";
+    public const string lv2_005 = "lv2-005";
+    public const string lv2_006 = "lv2-006";
+    public const string lv2_007 = "lv2-007";
+    public const string lv2_008 = "lv2-008";
+    public const string lv2_009 = "lv2-009";
+    public const string lv2_010 = "lv2-010";
+    public const string lv2_011 = "lv2-011";
+    public const string lv2_012 = "lv2-012";
+    public const string lv2_013 = "lv2-013";
+    public const string lv2_014 = "lv2-014";
+    public const string lv2_015 = "lv2-015";
+    public const string lv2_016 = "lv2-016";
 }
